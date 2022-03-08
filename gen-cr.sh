0e97ad8ea47f8a179a632a5bdc9719bc48370f68
Jingfang Liu <jingfangliu@google.com>
2022-03-08 09:47:54 -0800

    add disabled management

--- gen-cr.sh
+++ gen-cr.sh
@@ -10,7 +10,7 @@ metadata:
   name: crontab-ns
 EOF
 
-for n in {1..100}
+for n in {1..50}
 do
   cat >> configs/crs.yaml <<EOF
 ---
@@ -19,6 +19,24 @@ kind: CronTab
 metadata:
   name: cr-${n}
   namespace: crontab-ns
+spec:
+  cronSpec: "* * * * */5"
+  extra: field
+EOF
+done
+
+
+for n in {51..100}
+do
+  cat >> configs/crs.yaml <<EOF
+---
+apiVersion: "stable.example.com/v1"
+kind: CronTab
+metadata:
+  name: cr-${n}
+  namespace: crontab-ns
+  annotations:
+    configmanagement.gke.io/managed: disabled
 spec:
   cronSpec: "* * * * */5"
   extra: field
