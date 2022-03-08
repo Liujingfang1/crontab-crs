da04fa87638f354d80cae467d09621b097fbdb14
Jingfang Liu <jingfangliu@google.com>
2022-03-08 09:51:41 -0800

    add resources

--- gen-cr.sh
+++ gen-cr.sh
@@ -10,21 +10,6 @@ metadata:
   name: crontab-ns
 EOF
 
-for n in {1..50}
-do
-  cat >> configs/crs.yaml <<EOF
----
-apiVersion: "stable.example.com/v1"
-kind: CronTab
-metadata:
-  name: cr-${n}
-  namespace: crontab-ns
-spec:
-  cronSpec: "* * * * */5"
-  extra: field
-EOF
-done
-
 
 for n in {51..100}
 do
@@ -35,8 +20,6 @@ kind: CronTab
 metadata:
   name: cr-${n}
   namespace: crontab-ns
-  annotations:
-    configmanagement.gke.io/managed: disabled
 spec:
   cronSpec: "* * * * */5"
   extra: field
